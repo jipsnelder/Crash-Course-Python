cfedefa9c23d0ff3be39bcb872e1b92bf5109003
jipsnelder <jipsnelder@gmail.com>
2022-03-03 22:34:39 +0100

    Add files via upload

---
+++ full_name.py
@@ -0,0 +1,20 @@
+message = 'this is my first program'
+print(message)
+
+message = 'test test'
+print(message.title())
+print(message.upper())
+
+first_name = 'jip'
+last_name = 'sNelder'
+full_name = f"{first_name} {last_name}"
+print(f"\tHello, \n{full_name.title()}!")
+message = (f"Hello, {full_name.title()}!")
+print(message)
+
+favorite_language = 'python '
+print(favorite_language)
+print(favorite_language.rstrip())
+favorite_language = favorite_language.rstrip()
+print(favorite_language)
+
